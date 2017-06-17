e62536f37f437b5183ffb05979b6ae23af48e4a3
Jimmy Börjesson <alcesleo@gmail.com>
2017-06-17 20:20:37 +0200

    Move BottleCount class to the bottom of the file

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,4 +1,24 @@
 class Bottles
+  def song
+    verses(99, 0)
+  end
+
+  def verses(from, to)
+    from.downto(to).map { |count| verse(count) }.join("\n")
+  end
+
+  def verse(count)
+    bottle_count = BottleCount.new(count)
+    next_bottle_count = BottleCount.new(bottle_count.succ)
+
+    "#{bottle_count} of beer on the wall, ".capitalize +
+    "#{bottle_count} of beer.\n" +
+    "#{bottle_count.action}, " +
+    "#{next_bottle_count} of beer on the wall.\n"
+  end
+end
+
+class Bottles
   class BottleCount
     attr_reader :number
     private :number
@@ -52,22 +72,4 @@ class Bottles
     end
   end
   private_constant :BottleCount
-
-  def song
-    verses(99, 0)
-  end
-
-  def verses(from, to)
-    from.downto(to).map { |count| verse(count) }.join("\n")
-  end
-
-  def verse(count)
-    bottle_count = BottleCount.new(count)
-    next_bottle_count = BottleCount.new(bottle_count.succ)
-
-    "#{bottle_count} of beer on the wall, ".capitalize +
-    "#{bottle_count} of beer.\n" +
-    "#{bottle_count.action}, " +
-    "#{next_bottle_count} of beer on the wall.\n"
-  end
 end
